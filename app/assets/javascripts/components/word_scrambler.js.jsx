549e5ea267971b0b4420c8b1846b7973237fdebe
discotroll65 <elgaricimo@gmail.com>
2015-07-28 23:07:56 -0700

    word displays in blocks

--- app/assets/javascripts/components/word_scrambler.js.jsx
+++ app/assets/javascripts/components/word_scrambler.js.jsx
@@ -1,20 +1,26 @@
 class WordScrambler extends React.Component{
   render(){
+    let letters = this.props.ltrs;
     return(
       <div className="group word-scrambler">
-        <LetterBlock ltr="G"/>
-        <LetterBlock ltr="A"/>
-        <LetterBlock ltr="R"/>
-        <LetterBlock ltr="R"/>
-        <LetterBlock ltr="R"/>
-        <LetterBlock ltr="E"/>
-        <LetterBlock ltr="T"/>
-        <LetterBlock ltr="T"/>
+        {letters.map(function(letter, idx){
+          return <LetterBlock key={idx} ltr={letter}/>;
+        })}
       </div>
     );
   }
 }
 
-$(function () {
-  React.render(<WordScrambler/>, $('#app')[0]);
+var api = "http://api.wordnik.com/v4/words.json/randomWord?"
+api = api + "hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1"
+api = api + "&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5"
+api = api + "&maxLength=7"
+api = api + "&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+
+var response = $.ajax(apiUrl, {
+  dataType: "json",
+  success: function(){
+    var letters = response.responseJSON.word.split('');
+    React.render(<WordScrambler ltrs={letters}/>, $('#app')[0]);
+  }
 });
