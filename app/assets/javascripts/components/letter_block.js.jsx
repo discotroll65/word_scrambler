f269581f8b4b54e5cbd0b20bd57544b3d457be35
discotroll65 <elgaricimo@gmail.com>
2015-08-02 20:41:30 -0700

    some letters will highlight, can sense keyboard

--- app/assets/javascripts/components/letter_block.js.jsx
+++ app/assets/javascripts/components/letter_block.js.jsx
@@ -1,7 +1,8 @@
 class LetterBlock extends React.Component{
   render(){
+    var classes = (this.props.highlighted) ? "letter-block highlighted" : "letter-block" ;
     return(
-      <div className="letter-block">
+      <div className={classes}>
         <h1>{this.props.ltr}</h1>
       </div>
     );
--- app/assets/javascripts/components/word_scrambler.js.jsx
+++ app/assets/javascripts/components/word_scrambler.js.jsx
@@ -1,26 +1,38 @@
 class WordScrambler extends React.Component{
-  shuffle(arr){
-    var currentIndex = arr.length, tempVal, randIndex;
-
-    while(currentIndex > 0){
-      randIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      tempVal = arr[currentIndex];
-      arr[currentIndex] = arr[randIndex];
-      arr[randIndex] = tempVal;
-    }
-    return arr;
-  }
+  // constructor() {
+  //   super();
+  //
+  //   this.state = {
+  //     ltrIdx: 0,
+  //     ltrs: [],
+  //     shuffLtrs: []
+  //   };
+  //
+  //   this._handleKeyUp = this._handleKeyUp.bind(this);
+  // }
+  //
+  // _handleKeyUp(event){
+  //   debugger
+  //   if (event.keyCode === 8){
+  //     console.log('backspace');
+  //   } else{
+  //     console.log(String.fromCharCode(event.keyCode));
+  //   }
+  // }
 
   render(){
-    console.log(this.props.ltrs);
-    let letters = this.shuffle(this.props.ltrs);
-
+    console.log('word is: ' + this.props.ltrs);
+    let letters = this.props.shuffLtrs;
+    var that = this;
     return(
-      <div className="group word-scrambler">
+      <div className="group word-scrambler" onKeyUp={this._handleKeyUp} >
         {letters.map(function(letter, idx){
-          return <LetterBlock key={idx} ltr={letter}/>;
+          if(that.props.currentIdx > idx){
+            return <LetterBlock highlighted={true} key={idx} ltr={letter}/>;
+          } else {
+            return <LetterBlock key={idx} ltr={letter}/>;
+          }
+
         })}
       </div>
     )
@@ -35,10 +47,44 @@ api += "&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5"
 api += "&maxLength=7"
 api += "&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
 
+var currentIdx = 3, shuffledLtrs, letters;
+
 var response = $.ajax(api, {
   dataType: "json",
   success: function(){
-    var letters = response.responseJSON.word.split('');
-    React.render(<WordScrambler ltrs={letters}/>, $('#app')[0]);
+    letters = response.responseJSON.word.split('');
+    shuffledLtrs = (function(arr){
+      var currentIndex = arr.length, tempVal, randIndex;
+
+      while(currentIndex > 0){
+        randIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        tempVal = arr[currentIndex];
+        arr[currentIndex] = arr[randIndex];
+        arr[randIndex] = tempVal;
+      }
+      return arr;
+    })(letters.slice());
+    React.render(<WordScrambler currentIdx={currentIdx}
+       shuffLtrs={shuffledLtrs} ltrs={letters}/>, $('#app')[0]);
   }
+});
+
+$(function(){
+  $(document).keydown(function(event){
+    event.preventDefault();
+  });
+
+  $(document).keyup(function(event){
+    event.preventDefault();
+    if(event.keyCode === 8){
+      console.log("backspace")
+    } else{
+      console.log(String.fromCharCode(event.keyCode));
+    }
+
+    console.log(letters);
+    console.log(shuffledLtrs);
+  });
 });
